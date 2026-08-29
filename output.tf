f96cceca21db725d8415c39820742b922d036cd1
Sourish <mukherjee.sourish36@gmail.com>
2026-08-30 00:49:40 +0530

    feat:added name and public ip in output

--- output.tf
+++ output.tf
@@ -1,5 +1,8 @@
 output "aws-public-ip" {
   value = [
-    for instance in aws_instance.example : instance.public_ip
+    for instance in aws_instance.example : {
+      public_ip = instance.public_ip
+      name = instance.tags.Name
+    }
   ]
-}
+  }
